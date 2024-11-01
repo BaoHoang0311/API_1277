80539b7fc51596b58b2e55d918baaf4b90a6b1c3
baohoang031111 <tan.thach36@gmail.com>
2024-11-02 00:12:13 +0700

    ,

--- API127/obj/Debug/net7.0/API127.AssemblyInfo.cs
+++ API127/obj/Debug/net7.0/API127.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API127")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86768b97fb6f5cfe0c602462bfdd9802d45d3efa")]
 [assembly: System.Reflection.AssemblyProductAttribute("API127")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API127")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
